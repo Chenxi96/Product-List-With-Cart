aa53ddea00a78c4ebe7ec99e149e55934a29aa35
Chenxi Lin <lin.chenxi14@gmail.com>
2025-08-18 14:03:52 -0400

    Initialized Repo

---
+++ script/script.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    // Fetch json file from directory
+    async function fetchData() {
+        fetch('./data.json')
+        .then(response => response.json()) // Converts JSON file
+        .then(data => {
+            // Loop through json object
+            for(let item in data) {
+                const div = $('<div></div>'); // Container
+                const image = $(`<img />`).attr('src', data[item].image.mobile); // Image
+                const button = $('<button>Add to Cart</button>') // Button
+                const heading = $(`<h2>${data[item].category}</h2>`); // Heading
+                const name = $(`<p>${data[item].name}</p>`); // Name
+                const price = $(`<p>${data[item].price}</p>`); // Price
+                // Add each elements in div element
+                div.append(image);
+                div.append(button);
+                div.append(heading);
+                div.append(name);
+                div.append(price);
+                // Add the div into main element
+                $('main').append(div);
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    fetchData();
+}
