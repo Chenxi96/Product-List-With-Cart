091c481aff16d78966a4145139df0cec4d7127de
Chenxi Lin <lin.chenxi14@gmail.com>
2025-08-20 20:53:04 -0400

    Add style to tablet and desktop

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ window.onload = () => {
         fetch('./data.json')
         .then(response => response.json()) // Converts JSON file
         .then(data => {
-            // Loop through json object
+            const itemDiv = $('<div></div>').attr('id', "item");
             for(let item in data) {
                 const div = $('<div></div>'); // Container
                 const picture = $('<picture></picture>') // Picture element
@@ -12,9 +12,9 @@ window.onload = () => {
                 const tabletSource = $('<source>').attr({media: '(min-width: 375px)', srcset: data[item].image.tablet}) // Tablet image
                 const image = $(`<img />`).attr({src: data[item].image.mobile, style: 'max-width:100%'}); // Image
                 const button = $('<button>Add to Cart</button>').attr('class', 'add-cart-btn text-preset-4') // Button
-                const heading = $(`<h2>${data[item].category}</h2>`); // Heading
-                const name = $(`<p>${data[item].name}</p>`); // Name
-                const price = $(`<p>$ ${data[item].price.toFixed(2)}</p>`); // Price
+                const heading = $(`<h2>${data[item].category}</h2>`).attr({class: 'text-preset-4', style: 'color: var(--rose-500); margin: -12px 0 0 0;'}); // Heading
+                const name = $(`<p>${data[item].name}</p>`).attr({class: 'text-preset-3', style: 'color: var(--rose-900); margin: 4px 0;'}); // Name
+                const price = $(`<p>$${data[item].price.toFixed(2)}</p>`).attr({class: 'text-preset-3', style: 'color: var(--red); margin: 0;'}); // Price
 
                 // Add each elements in div element
                 picture.append(desktopSource)
@@ -25,9 +25,10 @@ window.onload = () => {
                 div.append(heading);
                 div.append(name);
                 div.append(price);
-                // Add the div into main element
-                $('main').append(div);
+                itemDiv.append(div);
 
+                // Add the div into main element
+                $('h1').after(itemDiv);
             }
         })
         .catch(error => {
